813e65d4b18981ae4f64dc2f4cf9129f9dfda5b7
Andrey Grebenshchikov <andrey19821975@gmail.com>
2020-03-10 15:20:36 +0200

    finish

--- test.php
+++ test.php
@@ -2,3 +2,41 @@
 /*
  * Plugin Name: Test
  */
+
+add_action('rest_api_init', function () {
+    register_rest_route('wp/v1', 'podcasts', array(
+        'methods' => 'GET',
+        'callback' => 'get_podcast',
+    ));
+});
+
+function get_podcast()
+{
+    $args = [
+        'post_type' => 'post',
+        'numberposts' => 99999,
+        'category_name'  => 'podcast'
+    ];
+
+    $posts = get_posts($args);
+
+    $data = [];
+
+    $i = 0;
+
+    foreach ($posts as $post) {
+        $data[$i]['title'] = $post->post_title;
+
+        $i++;
+    }
+
+    if (empty($data)) {
+        $response = new WP_REST_Response($data, 418);
+    } else {
+        $response = new WP_REST_Response($data, 200);
+    }
+
+
+
+    return $response;
+}
